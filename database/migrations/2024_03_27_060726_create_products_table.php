9e5ecccc4a7ac910ae0d588bcca4416757be8c87
Sharad Aghara <sharadaghara24@gmail.com>
2024-05-08 11:44:56 +0530

    qnt and price limits updated

--- database/migrations/2024_03_27_060726_create_products_table.php
+++ database/migrations/2024_03_27_060726_create_products_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('qnt');
-            $table->decimal('price', 10, 2);
+            $table->integer('qnt', 12);
+            $table->decimal('price', 12, 2);
             $table->string('image');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
